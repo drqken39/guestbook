17ee803d54ccfd90fc48ee425440662ef83daed4
drqken <drqken39@gmail.com>
2022-05-01 13:12:35 +0200

    First data structure

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,12 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/hello/{name}', name: 'homepage')]
+    public function index(string $name = ''): Response
     {
+        $greet = '';
+        if ($name) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(<<<EOF
 <html>
     <body>
+        $greet
         <img src="/images/under-construction.gif" />
     </body>
 </html>
